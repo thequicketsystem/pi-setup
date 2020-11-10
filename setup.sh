e5b3f14ec9f78dafccc90d5ac35dc93bc34a7647
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-11-09 18:33:09 -0600

    Removed multiline string

--- setup.sh
+++ setup.sh
@@ -13,14 +13,7 @@ sudo systemctl restart dphys-swapfile
 # package lists by subsystem
 common = "python3"
 
-people_counting = "cmake build-essential pkg-config git libjpeg-dev libtiff-dev \
-                    libjasper-dev libpng-dev libwebp-dev libopenexr-dev \
-                    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
-                    libxvidcore-dev libx264-dev libdc1394-22-dev \
-                    libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev \
-                    libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 \
-                    libatlas-base-dev liblapacke-dev gfortran libhdf5-dev libhdf5-103 \
-                    python3-dev python3-pip python3-numpy" 
+people_counting = "cmake build-essential pkg-config git libjpeg-dev libtiff-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libdc1394-22-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 libatlas-base-dev liblapacke-dev gfortran libhdf5-dev libhdf5-103 python3-dev python3-pip python3-numpy" 
 
 database = "mariadb-server"
 
