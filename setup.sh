e04498d96fdaf7cf314779ea852535ed79829de7
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-11-17 19:48:22 -0600

    Added option to not change swap size

--- setup.sh
+++ setup.sh
@@ -7,26 +7,18 @@ sudo apt upgrade
 
 # increase swap space temporarily. This is important for compiling OpenCV, but
 # is also nice to have for the whole process.
-sudo sed -i -e 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
-sudo systemctl restart dphys-swapfile
-
-# package lists by subsystem
-common = "python3"
-
-people_counting = "cmake build-essential pkg-config git libjpeg-dev libtiff-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libdc1394-22-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 libatlas-base-dev liblapacke-dev gfortran libhdf5-dev libhdf5-103 python3-dev python3-pip python3-numpy" 
-
-database = "mariadb-server"
-
-error_signaling = "rpi_ws281x adafruit-circuitpython-neopixel"
+if [ "$1" != "-ns" ];
+then
+    sudo sed -i -e 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
+    sudo systemctl restart dphys-swapfile
+fi
 
 # list of subsystems for which packages need to be installed
 subsystems = (common people_counting database error_signaling)
 
 # ---PACKAGE INSTALLATION---
-for subsystem_packages in ${subsystems[*]}
-do
-    sudo apt install $subsystem_packages
-done
+sudo apt install "rpi_ws281x adafruit-circuitpython-neopixel mariadb-server python3 cmake build-essential pkg-config git libjpeg-dev libtiff-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libdc1394-22-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 libatlas-base-dev liblapacke-dev gfortran libhdf5-dev libhdf5-103 python3-dev python3-pip python3-numpy"
+
 
 # ---OPENCV COMPILATION AND INSTALLATION---
 # retrieve the necessary repositories
@@ -62,8 +54,11 @@ sudo ldconfig
 
 # ---CLEANUP---
 # put our swap size back to normal
-sudo sed -i -e 's/CONF_SWAPSIZE=2048/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
-sudo systemctl restart dphys-swapfile
+if [ "$1" != "-ns" ];
+then
+    sudo sed -i -e 's/CONF_SWAPSIZE=2048/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
+    sudo systemctl restart dphys-swapfile
+fi
 
 # remove the files we made
 cd ~
